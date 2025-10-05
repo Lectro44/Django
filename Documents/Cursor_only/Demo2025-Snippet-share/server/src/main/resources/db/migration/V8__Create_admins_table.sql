bbbcc5f50f2738524e287c62d508ddf9eea653ea
Lectro <sahillr10a@gmail.com>
2025-10-05 11:37:51 +0530

    Database hosted in Clever Cloud

--- Documents/Cursor_only/Demo2025-Snippet-share/server/src/main/resources/db/migration/V8__Create_admins_table.sql
+++ Documents/Cursor_only/Demo2025-Snippet-share/server/src/main/resources/db/migration/V8__Create_admins_table.sql
@@ -1,7 +1,11 @@
  CREATE TABLE IF NOT EXISTS admins (
      id UUID PRIMARY KEY,
      name VARCHAR(255) NOT NULL UNIQUE,
-     password VARCHAR(255) NOT NULL,
+     password VARCHAR(255) NOT NULL
  );
+
+-- INSERT INTO admins (id, name, password)
+-- VALUES (gen_random_uuid(), 'Sahil', '4444');
+
  
  
